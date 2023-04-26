7a0e26f1b63d2c265560bb4e847f87af6c17ba64
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-04-26 12:06:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Форма регистрации</title>
     <link rel="stylesheet" href="style.css">
