991337a7431b84e2f196f6f952ead3240b34add9
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-05-03 02:41:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             <!-- Добавьте остальные поля формы с сохранением значений из cookie -->
             <label for="name">Имя:</label>
             <?= showError('name') ?>
-            <input type="text" name="name" id="name" <?php if (!empty($nameClass)) echo 'class="' . $nameClass . '"'; ?> value="<?= getFieldValue('name') ?>">
+            <input type="text" name="name" id="name" <?php if ($nameError) {echo 'class="errorsi"';} ?> value="<?= getFieldValue('name') ?>">
 
             <label for="email">E-mail:</label>
             <?= showError('email') ?>
