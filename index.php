d5caaff3b5f3f7a96f41830024db378cf3b00de0
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-05-03 00:07:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -29,40 +29,29 @@
             <!-- Добавьте остальные поля формы с сохранением значений из cookie -->
             <label for="name">Имя:</label>
             <?= showError('name') ?>
-            <input type="text" name="name" id="name"  value="<?= getFieldValue('name') ?>">
+            <input type="text" name="name" id="name" value="<?= getFieldValue('name') ?>">
 
             <label for="email">E-mail:</label>
             <?= showError('email') ?>
-            <input type="text" name="email" id="email"  value="<?= getFieldValue('email') ?>">
+            <input type="text" name="email" id="email" value="<?= getFieldValue('email') ?>">
 
-<!--             <label for="birth_year">Год рождения:</label> -->
-           
-<!--             <input type="text" name="birth_year" id="birth_year" value="<?= getFieldValue('birth_year') ?>"> -->
-            <label for="birth_year">Год рождения:</label> 
+            <label for="birth_year">Год рождения:</label>
             <?= showError('birth_year') ?>
-            <select name="birth_year" id="birth_year" >
-                <option   value="<?= getSelected('birth_year',"") ?>">Выберите год</option>
-                <!-- Заполните значения для годов -->
-            </select>
+            <input type="text" name="birth_year" id="birth_year" value="<?= getFieldValue('birth_year') ?>">
 
             <label>Пол:</label>
             <?= showError('gender') ?>
-            <label><input type="radio" checked="checked" name="gender" value="male" <?= getChecked('gender', 'male') ?> />Мужской</label>
-			<label><input type="radio" name="gender" value="female" <?= getChecked('gender', 'female') ?> />Женский</label>
+            <label><input type="radio" name="gender" value="male" <?= getChecked('gender', 'male') ?>> Мужской</label>
+            <label><input type="radio" name="gender" value="female" <?= getChecked('gender', 'female') ?>> Женский</label>
 
             <label>Количество конечностей:</label>
             <?= showError('limbs') ?>
-            <label><input type="radio" name="limbs" value="2" <?=getSelected('limbs', '2')?>> 2</label>
-           <label><input type="radio" name="limbs" value="2" <?=getSelected('limbs', '4')?>> 4</label>
-            <label><input type="radio" name="limbs" value="6" <?=getSelected('limbs', '6')?>> 6</label>
-
+            <input type="text" name="limbs" id="limbs" value="<?= getFieldValue('limbs') ?>">
 <label for="abilities">Сверхспособности:</label>
             <select name="abilities[]" id="abilities" multiple>
                 <option value="immortality" <?= getSelected('abilities', 'immortality') ?>>Бессмертие</option>
                 <option value="wall_pass" <?= getSelected('abilities', 'wall_pass') ?>>Прохождение сквозь стены</option>
                 <option value="levitation" <?= getSelected('abilities', 'levitation') ?>>Левитация</option>
-                 <option value="Fly" <?= getSelected('abilities', 'Fly') ?>>Полёт</option>
-               <option value="tp" <?= getSelected('abilities', 'Teleportation') ?>>Телепортация</option>
             </select>
 
             <label for="bio">Биография:</label>
@@ -75,16 +64,5 @@
             <input type="submit" value="Отправить">
         </form>
     </div>
-     <script>
-        // Заполнение списка годов
-        const select = document.getElementById('birth_year');
-        const currentYear = new Date().getFullYear();
-        for (let i = currentYear; i >= currentYear - 100; i--) {
-            const option = document.createElement('option');
-            option.value = i;
-            option.text = i;
-            select.add(option);
-        }
-    </script>
 </body>
 </html>
